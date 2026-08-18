1d4f686ab8c3b4ac1135753d9c5bb08ed3877409
Kris Geusebroek <krisgeus@gmail.com>
2026-08-18 15:57:31 +0200

    Fix pre-commit

---
+++ PSScriptAnalyzer-wrapper.ps1
@@ -0,0 +1,54 @@
+[CmdletBinding()]
+Param(
+    # Analyze options
+    [Parameter(Mandatory = $true, ParameterSetName = 'Check', Position = 0)]
+    [Switch]$Check,
+    [Parameter(ParameterSetName = 'Check')]
+    [String[]]$CustomRulePath,
+    [Parameter(ParameterSetName = 'Check')]
+    [String[]]$ExcludeRule,
+    [Parameter(ParameterSetName = 'Check')]
+    [Switch]$Fix,
+    [Parameter(ParameterSetName = 'Check')]
+    [Switch]$IncludeDefaultRules,
+    [Parameter(ParameterSetName = 'Check')]
+    [String[]]$IncludeRule,
+    [Parameter(ParameterSetName = 'Check')]
+    [Switch]$IncludeSuppressed,
+    [Parameter(ParameterSetName = 'Check')]
+    [Switch]$RecurseCustomRulePath,
+    [Parameter(ParameterSetName = 'Check')]
+    [Switch]$SaveDscDependency,
+    [Parameter(ParameterSetName = 'Check')]
+    [String[]]$Severity,
+    # Format options
+    [Parameter(Mandatory = $true, ParameterSetName = 'Format', Position = 0)]
+    [Switch]$Format,
+    [Parameter(ParameterSetName = 'Format')]
+    [Int32[]]$Range,
+    # Common options
+    [Parameter(Mandatory = $true, ValueFromRemainingArguments = $true)]
+    [String[]]$Path,
+    [Parameter()]
+    [String]$Settings
+)
+
+if ([Environment]::GetEnvironmentVariable('PRE_COMMIT_PWSH_VERBOSE')) {
+    $VerbosePreference = 'Continue'
+}
+$ErrorActionPreference = 'Stop'
+
+[void]$PSBoundParameters.Remove($PsCmdlet.ParameterSetName)
+[void]$PSBoundParameters.Remove('Path')
+
+switch ($PsCmdlet.ParameterSetName) {
+    'Check' {
+        $Path | Invoke-ScriptAnalyzer -EnableExit @PSBoundParameters
+    }
+    'Format' {
+        $Path | ForEach-Object {
+            $content = (Get-Content -Raw -Path $_).Trim()
+            Invoke-Formatter -ScriptDefinition $content @PSBoundParameters | Out-File -FilePath $_
+        }
+    }
+}
